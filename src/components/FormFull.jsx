a2a35504935cd531d886cfc716d20f590309dc85
sabin prajapati <sabinprajapati7@gmail.com>
2022-08-01 16:51:55 +0545

    form

--- src/components/FormFull.jsx
+++ src/components/FormFull.jsx
@@ -5,21 +5,50 @@ export const FormFull = () => {
     <div className='form-container'>
       <form action='#'>
         <div className='leave-form'>
-          <div className='input-box'>
-            <span className='details'>Leave Type</span>
-            <input type={'text'} placeholder='Select Leave Type' required></input>
-          </div>
-          <div className='input-box'>
-            <span className='details'>Project Manager</span>
-            <input type={'text'} placeholder='Select Project Manager' required></input>
+        <div className="select-type">
+            <input type={"radio"} name="type" id="dot-1"></input>
+            <input type={"radio"} name="type" id="dot-2"></input>
+            <input type={"radio"} name="type" id="dot-3"></input>
+            <input type={"radio"} name="type" id="dot-4"></input>
+            <input type={"radio"} name="type" id="dot-5"></input>
+            <input type={"radio"} name="type" id="dot-6"></input>
+            <span className="details">Leave Type</span>
+            <div className="type-category">
+              <label for="dot-1">
+                <span className="dot one"></span>
+                <span className="select-leave">Annual Leave</span>
+              </label>
+              <label for="dot-2">
+                <span className="dot two"></span>
+                <span className="select-leave">Sick Leave</span>
+              </label>
+              <label for="dot-3">
+                <span className="dot three"></span>
+                <span className="select-leave">Emergency Leave</span>
+              </label>
+            </div>
+            <div className="type-category">
+              <label for="dot-4">
+                <span className="dot four"></span>
+                <span className="select-leave">Bereavement Leave</span>
+              </label>
+              <label for="dot-5">
+                <span className="dot five"></span>
+                <span className="select-leave">Personal Leave</span>
+              </label>
+              <label for="dot-6">
+                <span className="dot six"></span>
+                <span className="select-leave">Casual Leave</span>
+              </label>
+            </div>
           </div>
           <div className='input-box'>
             <span className='details'>From</span>
-            <input type={'Date'} required></input>
+            <input type={'date'} required></input>
           </div>
           <div className='input-box'>
             <span className='details'>To</span>
-            <input type={'date'} required></input>
+            <input type={'date'}></input>
           </div>
           <div className='reason-field'>
             <span className='details'>Reason</span>
--- src/components/FormHalf.jsx
+++ src/components/FormHalf.jsx
@@ -1,47 +1,75 @@
-import React from 'react'
+import React from "react";
 
 export const FormHalf = () => {
   return (
-    <div className='form-container'>
-      <form action='#'>
-        <div className='leave-form'>
-          <div className='input-box'>
-            <span className='details'>Leave Type</span>
-            <input type={'text'} placeholder='Select Leave Type' required></input>
-          </div>
-          <div className='select-time'>
-            <input type={'radio'} name='time' id='dot-1'></input>
-            <input type={'radio'} name='time' id='dot-2'></input>
-            <span className='time-range'>Time Range</span>
-            <div className='time-category'>
-              <label for="dot-1">
-                <span className='dot one'></span>
-                <span className='Select-half'>First Half</span>
+    <div className="form-container">
+      <form action="#">
+        <div className="leave-form">
+          <div className="select-type">
+            <input type={"radio"} name="type" id="dot-7"></input>
+            <input type={"radio"} name="type" id="dot-8"></input>
+            <input type={"radio"} name="type" id="dot-9"></input>
+            <input type={"radio"} name="type" id="dot-10"></input>
+            <input type={"radio"} name="type" id="dot-11"></input>
+            <input type={"radio"} name="type" id="dot-12"></input>
+            <span className="details">Leave Type</span>
+            <div className="type-category">
+              <label for="dot-7">
+                <span className="dot one"></span>
+                <span className="select-leave">Annual Leave</span>
               </label>
-              <label for="dot-2">
-                <span className='dot two'></span>
-                <span className='Select-half'>Second Half</span>
+              <label for="dot-8">
+                <span className="dot two"></span>
+                <span className="select-leave">Sick Leave</span>
+              </label>
+              <label for="dot-9">
+                <span className="dot three"></span>
+                <span className="select-leave">Emergency Leave</span>
+              </label>
+            </div>
+            <div className="type-category">
+              <label for="dot-10">
+                <span className="dot four"></span>
+                <span className="select-leave">Bereavement Leave</span>
+              </label>
+              <label for="dot-11">
+                <span className="dot five"></span>
+                <span className="select-leave">Personal Leave</span>
+              </label>
+              <label for="dot-12">
+                <span className="dot six"></span>
+                <span className="select-leave">Casual Leave</span>
               </label>
             </div>
           </div>
-          <div className='input-box'>
-            <span className='details'>Project Manager</span>
-            <input type={'text'} placeholder='Select Project Manager' required></input>
+          <div className="input-box">
+            <span className="details">Date</span>
+            <input
+              type={"Date"}
+              placeholder="Select Leave Type"
+              required
+            ></input>
           </div>
-          <div className='input-box'>
-            <span className='details'>Date</span>
-            <input type={'Date'} placeholder='Select Leave Type' required></input>
+          <div className="input-box">
+            <span className="details">Time Range</span>
+            <select required>
+              <option value="" disabled selected>
+                Select Time Range
+              </option>
+              <option value="Annual Leave">First Half</option>
+              <option value="Sick Leave">Second Half</option>
+            </select>
           </div>
-          <div className='reason-field'>
-            <span className='details'>Reason</span>
+          <div className="reason-field">
+            <span className="details">Reason</span>
             <textarea required></textarea>
           </div>
         </div>
-        <div className='button1'>
-          <input className='apply' type={'submit'} value='Apply'></input>
-          <input className='cancel' type={'reset'} value='Cancel'></input>
+        <div className="button1">
+          <input className="apply" type={"submit"} value="Apply"></input>
+          <input className="cancel" type={"reset"} value="Cancel"></input>
         </div>
       </form>
     </div>
-  )
-}
+  );
+};
