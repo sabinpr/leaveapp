8c82373e091a3af472f6e57d46abdde56e1738bf
sabin prajapati <sabinprajapati7@gmail.com>
2022-08-02 16:40:10 +0545

    Form validation

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,70 +1,69 @@
 import React from "react";
+import { Formik, Form } from "formik";
+import TextField from "../components/TextField";
+import * as Yup from "yup";
 
 const Register = () => {
+  const validate = Yup.object({
+    firstName: Yup.string()
+      .max(15, "Must be 15 characters or less")
+      .required("Required"),
+    lastName: Yup.string()
+      .max(20, "Must be 20 characters or less")
+      .required("Required"),
+    email: Yup.string()
+      .email("Email is invalid")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(8, "Password must be at least 8 charaters")
+      .required("Password is required"),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref("password"), null], "Password must match")
+      .required("Confirm password is required"),
+  });
   return (
-    <>
-      <div className="form-background">
-        <div className="Auth-form-container">
-          <div className="Auth-form">
-            <div className="Auth-form-content">
-              <div className="Auth-form-title">Register</div>
-              <div className="form-group mt-3">
-                <label>First name</label>
-                <input
-                  type="text"
-                  className="form-control mt-1"
-                  placeholder="Enter first name"
-                  required
-                />
-              </div>
-              <div className="form-group mt-3">
-                <label>Last name</label>
-                <input
-                  type="text"
-                  className="form-control mt-1"
-                  placeholder="Enter last name"
-                  required
-                />
-              </div>
-              <div className="form-group mt-3">
-                <label>Email address</label>
-                <input
-                  type="email"
-                  className="form-control mt-1"
-                  placeholder="Enter email"
-                  required
-                />
-              </div>
-              <div className="form-group mt-3">
-                <label>Password</label>
-                <input
-                  type="password"
-                  className="form-control mt-1"
-                  placeholder="Enter password"
-                  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                  required
-                />
-              </div>
-              <div className="form-group mt-3">
-                <label>Confirm Password</label>
-                <input
-                  type="password"
-                  className="form-control mt-1"
-                  placeholder="Enter password"
-                  pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                  required
-                />
-              </div>
-              <div className="d-grid gap-2 mt-4 mb-2">
-                <button type="submit" className="btn btn-primary btn-block">
-                  Register
-                </button>
+    <Formik
+      initialValues={{
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      }}
+      validationSchema={validate}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      {(formik) => (
+        <div className="form-background">
+          <div className="Auth-form-container">
+            <div className="Auth-form">
+              <div className="Auth-form-content">
+                <h3 className="Auth-form-title">Register</h3>
+                <Form>
+                  <TextField label="First Name" name="firstName" type="text" />
+                  <TextField label="Last Name" name="lastName" type="text" />
+                  <TextField label="Email" name="email" type="email" />
+                  <TextField label="Password" name="password" type="password" />
+                  <TextField
+                    label="Confirm Password"
+                    name="confirmPassword"
+                    type="password"
+                  />
+                  <button className="btn btn-dark mt-3 mb-2 mr-3" type="submit">
+                    Register
+                  </button>
+                  <button className="btn btn-danger mt-3 mb-2" type="reset">
+                    Reset
+                  </button>
+                </Form>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
+      )}
+    </Formik>
   );
 };
 
