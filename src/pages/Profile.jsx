3e9c563f0f8ba9ae84ea38e568d46b4d5badfee9
sabin prajapati <sabinprajapati7@gmail.com>
2022-08-09 14:00:10 +0545

    COMplete User Profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import MyProfile from '../components/MyProfile'
 import UserProfile from '../components/UserProfile'
 
 const Profile = () => {
   return (
     <>
       <div className='content-header'>
-        <h1>User Profile</h1>
+        <h1>My Profile</h1>
         <UserProfile />
       </div>
       <div className="content">
+        <MyProfile />
       </div>
     </>
   )
